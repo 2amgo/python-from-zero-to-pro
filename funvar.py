0d7bc4583c10f527767421346a2f08b323ebc3f2
2AMgo <phucthinh.sun@gmail.com>
2022-01-07 21:28:11 +0700

    add more file

---
+++ funvar.py
@@ -0,0 +1,5 @@
+# cac kieu du lieu co ban trong python
+# 1. kieu so nguyen, kieu so thuc , complex so ao
+#2. kieu string 3. kieu boolen #
+name = "Iron - man"
+print("Hello ! my name's : " + name)
